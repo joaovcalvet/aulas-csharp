c2339f482a24dd6327f2215b3d3b11a10123a2a1
João Victor Calvet <joaovictor.calvet@outlook.com>
2024-09-09 09:48:40 -0300

    [Feat] Criando a Classe Posicao

---
+++ Jogo de Xadrez/Entities/Tabuleiro/Posicao.cs
@@ -0,0 +1,13 @@
+namespace Entities.Tabuleiro;
+
+class Posicao
+{
+    public int Linha {get; set;}
+    public int Coluna {get; set;}
+
+    public Posicao(int linha, int coluna)
+    {
+        this.Linha = linha;
+        this.Coluna = coluna;
+    }
+}
